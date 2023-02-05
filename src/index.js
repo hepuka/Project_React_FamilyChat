c02f52b2c9ff1db9362143ae5667a9098148cc97
Kun-Fagyal Zoltan <kunzoltan7707@gmail.com>
2023-02-05 10:41:47 +0100

    upd

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
 import { ChatContextProvider } from "./context/ChatContext";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <AuthContextProvider>
-    <ChatContextProvider>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </ChatContextProvider>
-  </AuthContextProvider>
+  <HashRouter>
+    <AuthContextProvider>
+      <ChatContextProvider>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </ChatContextProvider>
+    </AuthContextProvider>
+  </HashRouter>
 );
